66156dd1bdc4aa55eb6ffa62a4c7160d44477ac6
Gregory D. Toprak & Wesley El-Amin <pair+gdtoprak.wesleye2003@devbootcamp.com>
2016-05-10 14:26:50 -0500

    Complete board model functions and their tests

--- js/board.js
+++ js/board.js
@@ -14,18 +14,70 @@ Board.prototype.topToBottom = function() {
       this.stuff[j][i] = temp
     }
   }
-  return this.stuff
 }
 
 Board.prototype.rightToLeft = function() {
   for (var i in this.stuff) {
     this.stuff[i] = this.stuff[i].reverse();
   }
-  return this.stuff
 }
 
 Board.prototype.bottomToTop = function() {
   this.topToBottom();
-  this.rightToLeft();
-  return this.stuff;
+  this.rightToLeft();;
+}
+
+//////// Push it
+
+Board.prototype.pushThisWay = function() {
+  for (var i in this.stuff) {
+    rowPush(this.stuff[i])
+  }
+}
+
+function rowPush(row) {
+  for (var i = 2; i >= 0; i--) {
+    if (row[i] === row[i+1]) {
+      row[i+1] *= 2
+      row[i] = 0
+    } else if (row[i+1] === 0) {
+      row[i+1] = row[i]
+      row[i] = 0
+      rowPush(row);
+    }
+  }
+}
+
+//////// Bools
+
+Board.prototype.canPushThisWay = function() {
+  return this.stuff.some(rowCanPush)
+}
+
+function rowCanPush(array) {
+  var oldArray = arrCopy(array)
+  rowPush(oldArray)
+  return !arrEqual(array, oldArray)
+};
+
+//////// Helper methods
+
+function arrEqual(a,b) {
+  if (a.length !== b.length) {
+    return false
+  }
+  for (var i in a) {
+    if (a[i] !== b[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+function arrCopy(a) {
+  var b = []
+  for (var i in a) {
+    b.push(a[i])
+  }
+  return b
 }
--- js/game.js
+++ js/game.js
@@ -1,3 +1,9 @@
 function Game() {
   this.board = new Board()
 }
+
+// Board.prototype.canPushAny
+// this.rightToLeft
+// if (this.canPushThisWay())
+  // loop and push all rows
+  // generate new number
